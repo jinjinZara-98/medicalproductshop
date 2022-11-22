051be0cd1820b50f3beeb3c3a93f3164fb9e1325
captainHeo <heojin0620@naver.com>
2022-11-22 21:52:02 +0900

    update

--- src/main/java/capstonedesign/medicalproduct/AwsS3Config.java
+++ src/main/java/capstonedesign/medicalproduct/AwsS3Config.java
@@ -9,16 +9,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class AwsS3Config {
 
-//    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-//    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-//    @Value("${cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     //빈으로 등록
